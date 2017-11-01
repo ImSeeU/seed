49d9463a8d58c4b5bd333e2793f90560896039f1
Arjun <arjuntheprakash@gmail.com>
2017-11-01 04:36:32 +0000

    ocr writes

--- seed/src/services/ocr.js
+++ seed/src/services/ocr.js
@@ -1,12 +1,22 @@
-// Assume we've got an image loaded
-
 console.log('jello');
 
   var Tesseract = require('tesseract.js')
   const path = require("path");
   var imagePath= path.join(__dirname,"bond.jpg");
-
+  var fs = require('fs');
+  
   Tesseract.create({
     langPath: path.join(__dirname, "langs")
   }).recognize(imagePath, {lang: "eng"}) // Or whichever lang you have downloaded to langs/
-      .then((result) => console.log(result.text));
+      .then((result) => fs.writeFile("test.txt", result.text, function(err) {
+      if(err) {
+          return console.log(err);
+      }
+  
+      console.log("The file was saved!");
+  }));
+      
+  
+  
+      
+      
